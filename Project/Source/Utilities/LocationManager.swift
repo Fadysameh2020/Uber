3963bf4e0ae7f574bb47e75735cb16fb30bdf36e
Fadysameh2020 <fadysameh10@gmail.com>
2024-07-08 11:21:26 +0300

    fisrt commit

---
+++ Project/Source/Utilities/LocationManager.swift
@@ -0,0 +1,8 @@
+//
+//  LocationManager.swift
+//  Uber
+//
+//  Created by Fady Sameh on 7/8/24.
+//
+
+import Foundation
---
+++ Project/Source/ViewRelated/Modules/HomeModule/View/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  Uber
+//
+//  Created by Fady Sameh on 7/8/24.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
